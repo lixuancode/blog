284148075904c3b5616d5705d9b5e9d34f2e47f1
codedrinker <50828441@qq.com>
2019-07-31 18:48:44 +0800

    add h2database

--- src/main/java/net/blog/w9o/blog/controller/AuthorizeController.java
+++ src/main/java/net/blog/w9o/blog/controller/AuthorizeController.java
@@ -4,26 +4,43 @@ import net.blog.w9o.blog.dto.AccessTokenDto;
 import net.blog.w9o.blog.dto.GithubUser;
 import net.blog.w9o.blog.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.url}")
+    private String RedirectUrl;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){ //RequestParam接收参数
+                           @RequestParam(name="state")String state, HttpServletRequest request)
+    { //RequestParam接收参数
         AccessTokenDto accessTokenDto = new AccessTokenDto();
-        accessTokenDto.setClient_id("Iv1.7b3265842a4b3ab9");
-        accessTokenDto.setClient_secret("54c1735f4b7ecf93717b6d433649aee02c5940e7");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_url("http://localhost:8887/callback");
+        accessTokenDto.setRedirect_url(RedirectUrl);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";//登录成功之后返回index页面
+        if(user!=null){
+            //登录成功写cookie 与 session
+            request.getSession().setAttribute("user",user);
+            return  "redirect:/";//重定向跳转到index页面
+
+        }else {
+            //登录失败
+            return "redirect:/";//重定向跳转到index页面
+        }
     }
 }
