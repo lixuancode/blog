7782c8bbfc4f1a5d545935a6d0ac46946766a7a3
codedrinker <50828441@qq.com>
2019-08-14 20:20:59 +0800

    修复数据库

--- src/main/resources/db/migration/v12__Change_id_column_type.sql
+++ src/main/resources/db/migration/v12__Change_id_column_type.sql
@@ -1,2 +1,2 @@
-alter table QUESTION alter column ID AUTO_INCREMENT  NOT NULL ;
-alter table USER alter column ID AUTO_INCREMENT  NOT NULL ;
+alter table QUESTION alter column ID BIGINT AUTO_INCREMENT;
+alter table USER alter column ID BIGINT AUTO_INCREMENTgit;
