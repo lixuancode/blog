e5d90de75deaeda0bd42acb342a8b1370402d74e
codedrinker <50828441@qq.com>
2019-08-15 19:57:13 +0800

    修复

--- src/main/java/net/blog/w9o/blog/BlogApplication.java
+++ src/main/java/net/blog/w9o/blog/BlogApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("net.blog.w9o.blog.mapper")
-//git@EnableScheduling
+@EnableScheduling
 public class BlogApplication {
 
     public static void main(String[] args) {
