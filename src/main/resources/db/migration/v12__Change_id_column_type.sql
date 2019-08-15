76585f135d7ef088296f905e87d40faaffdd1d89
codedrinker <50828441@qq.com>
2019-08-15 09:54:54 +0800

    mysql

--- src/main/resources/db/migration/v12__Change_id_column_type.sql
+++ src/main/resources/db/migration/v12__Change_id_column_type.sql
@@ -1,2 +1,2 @@
-alter table question alter column id bigint auto_increment;
-alter table user alter column id bigint auto_increment;
+alter table question modify id bigint auto_increment not null ;
+alter table user modify id bigint auto_increment not null ;
